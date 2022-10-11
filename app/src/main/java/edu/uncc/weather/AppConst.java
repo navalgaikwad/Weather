d1e5aac43b138faa3a4b0371c237403bcb2b927f
navalgaikwad <navalgaikwad1991@outlook.com>
2022-10-11 15:41:34 -0400

    Update AppConst.java
    Added the corrections

--- app/src/main/java/edu/uncc/weather/AppConst.java
+++ app/src/main/java/edu/uncc/weather/AppConst.java
@@ -4,5 +4,5 @@ public class AppConst {
 
     static String ICON_API_URL="https://openweathermap.org/img/wn/";
     public static final String HTTPS_API_OPENWEATHERMAP_ORG_DATA_2_5_FORECAST_Q = "https://api.openweathermap.org/data/2.5/forecast?q=";
-    public static final String API_KEY = "4241353f49f5b2a7b9217d2829429568";
+    public static final String API_KEY = "";
 }
